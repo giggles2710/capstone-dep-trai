414574f0fbf08b185fef73da5cebe682603d4aec
giggles.2710@gmail.com <giggles.2710@gmail.com@641260e3-7f25-cf5c-649d-fd4aaedfc06c>
2013-12-22 03:24:41 +0000

    update user schema

--- trunk/user.js
+++ trunk/user.js
@@ -1,94 +1,73 @@
 /**
  * Created by ConMeoMauDen on 15/12/2013.
+ * 
+ * Updated by Noir on 22/12/2013
  */
 // Mongoose initialize:
-var mongoose = require('mongoose')
-mongoose.connect('mongodb://127.0.0.1/test')
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
 
-
-var userSchema = {
-    username:    {
+var userSchema = new Schema({
+    username:  {
         type: String,
-        required: true
+        required: true,
+        index: {unique: true}
     },
     password:  {
         type: String,
         required: true
     },
-    email:     {
-        type: String,
-        required: true
+    email: {
+        type: String, 
+        required: true,
+        lowercase: true
     },
     fullname: {
         type: String,
+        required: true,
+        lowercase: true
+    },
+    birthday:{
+        type: Date, 
         required: true
     },
-    birthday:   {
-        type: Date,
-        required: false
-    },
-    gender:   {
+    gender: {
         type: String,
-        required: true
+        required: true,
+        lowercase: true,
+        enum: ['male','female']
     },
-    avatar: String,
-    aboutme: String,
+    avatar:String,
+    aboutMe: String,
     location: String,
     workplace: String,
-    banned: Boolean,
-    createddate: Date,
-    friend: [{
+    isBanned: Boolean,
+    createDate: {
+        type: Date,
+        default: Date.now
+    },
+    friend:[{
         username: String,
         fullname: String,
         avatar: String
     }],
-    group: {
+    group:{
         name: String,
-        listuser: [String]
+        listUser: [String]
     },
-    todolist:{
+    todoList:{
         name: String,
         type: String,
-        chore:{
+        chore: {
             content: String,
             status: String,
-            createddate: Date,
-            donedate: Date
+            createDate:{
+                type: Date,
+                default: Date.now
+            },
+            doneDate: Date
         }
-    }
-
-}
-User = mongoose.model('User', userSchema);
-
-// Model instances:
-var User = new User({
-    username:'SE80088',
-    password:'123456789',
-    email: 'trung@yahoo.com',
-    fullname:'Nguyen Minh Trung',
-    gender:'Male',
-    location:'Da Nang',
-    friend: [{
-        username: 'SE90090',
-        fullname:'Tran Nhat Minh'
-    }, {
-        username: 'SE90089',
-        fullname:'Dinh Minh Chau'
-    }],
-    group:  {
-    name:'FUD',
-    listuser: ['SE90012','SE90013','SE90015']
     }
 });
 
-//Save to Database
-User.save(function(err){
-    if (err){
-        console.log('Error !');
-    } else {
-        console.log('Saved');
-    }
-
-    }
-)
-
+module.exports = mongoose.model('User', userSchema);
