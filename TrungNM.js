432c5fce2806dfc8425596a6416492b513007279
trungnmse90088 <trungnmse90088@fpt.edu.vn>
2013-12-22 15:47:23 +0000

    Trung đang tạo linh tinh !

---
+++ TrungNM.js
@@ -0,0 +1,19 @@
+/**
+ * Created by ConMeoMauDen on 22/12/2013.
+ */
+var express = require('express');
+var app = express();
+
+// Express configurations
+app.configure(function(){
+    app.set('port', process.env.PORT || 3000);
+    app.set('views', __dirname + '/views/TrungNM');
+    app.set('view engine', 'ejs');
+    app.use(app.router);
+});
+
+app.get('/', function(req, res) {
+    res.render('index', {title: 'Hanoijs'});
+});
+
+app.listen(app.get('port'));
