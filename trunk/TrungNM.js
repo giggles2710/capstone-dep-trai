eeb5a565afcce0425dc6050e07b0c6adae98f645
trungnmse90088@fpt.edu.vn <trungnmse90088@fpt.edu.vn@641260e3-7f25-cf5c-649d-fd4aaedfc06c>
2013-12-24 01:13:08 +0000

    Trung đang tạo linh tinh !

--- trunk/TrungNM.js
+++ trunk/TrungNM.js
@@ -1,19 +1,62 @@
+
 /**
- * Created by ConMeoMauDen on 22/12/2013.
+ * Module dependencies.
  */
+
 var express = require('express');
 var app = express();
 
-// Express configurations
-app.configure(function(){
-    app.set('port', process.env.PORT || 3000);
-    app.set('views', __dirname + '/views/TrungNM');
-    app.set('view engine', 'ejs');
-    app.use(app.router);
+// Mongoose initialize:
+
+
+var express = require('express');
+var routes = require('./routes');
+var user = require('./routes/user');
+var http = require('http');
+var path = require('path');
+
+var app = express();
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views/TrungNM'));
+app.set('view engine', 'ejs');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Test
+app.configure(function() {
+    app.use(express.errorHandler({
+        dumpExceptions: true,
+        showStack: true
+    }));
 });
 
-app.get('/', function(req, res) {
-    res.render('index', {title: 'Hanoijs', a : 'sida'});
-});
 
-app.listen(app.get('port'));
+// development only
+if ('development' == app.get('env')) {
+    app.use(express.errorHandler());
+}
+
+app.get('/', routes.index);
+// This is mine !
+
+app.get('/view',routes.view);
+app.get('/addnew',routes.addnew);
+
+app.get('/del',routes.del);
+
+app.get('/delete/:userID', routes.delete);
+
+app.post('/newuser', routes.newuser);
+
+
+
+http.createServer(app).listen(app.get('port'), function(){
+    console.log('Express server listening on port ' + app.get('port'));
+});
---
+++ trunk/routes/user.js
@@ -0,0 +1,69 @@
+///**
+// * Created by ConMeoMauDen on 22/12/2013.
+// */
+//
+//
+//// User Schema
+//
+//var userSchema = new Schema({
+//    username:  {
+//        type: String,
+//        required: true,
+//        index: {unique: true}
+//    },
+//    password:  {
+//        type: String,
+//        required: true
+//    },
+//    email: {
+//        type: String,
+//        required: true,
+//        lowercase: true
+//    },
+//    fullname: {
+//        type: String,
+//        required: true,
+//        lowercase: true
+//    },
+//    birthday:{
+//        type: Date,
+//        required: true
+//    },
+//    gender: {
+//        type: String,
+//        required: true,
+//        lowercase: true,
+//        enum: ['male','female']
+//    },
+//    avatar:String,
+//    aboutMe: String,
+//    location: String,
+//    workplace: String,
+//    isBanned: Boolean,
+//    createDate: {
+//        type: Date,
+//        default: Date.now
+//    },
+//    friend:[{
+//        username: String,
+//        fullname: String,
+//        avatar: String
+//    }],
+//    group:{
+//        name: String,
+//        listUser: [String]
+//    },
+//    todoList:{
+//        name: String,
+//        type: String,
+//        chore: {
+//            content: String,
+//            status: String,
+//            createDate:{
+//                type: Date,
+//                default: Date.now
+//            },
+//            doneDate: Date
+//        }
+//    }
+//});
