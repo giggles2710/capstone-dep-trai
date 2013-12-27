92dd10eb200bb7a25dd3d3580b584f68077089f9
trungnmse90088 <trungnmse90088@fpt.edu.vn>
2013-12-27 06:08:01 +0000

    Update ngày 27/1/2013

--- routes/users/setting.js
+++ routes/users/setting.js
@@ -7,140 +7,153 @@ var User = require(path.join(HOME + "/models/user"));
 var helper = require(path.join(HOME + "/helpers/helper"));
 var validator = require(path.join(HOME + "/helpers/userValidator"));
 
-// View All
-exports.view = function (req, res) {
-    req.session.user = {
-        _id: '52ba9a7637d5e99c05000003',
-        fullName: 'Nguyễn Minh Trung',
-        provider: 'facebook'
-    }
-    User.find(function (err, data) {
-        if (!err) {
-            res.render('view', { title: 'View All User', data: data});
-        } else {
-            console.log('Error');
-        }
-    })
-};
 
-// Delete User
-exports.delete = function (req, res) {
-    var id = req.params.userID;
-    console.log(id);
-    User.remove({_id: id}, function (err) {
-        if (!err) {
-            console.log('Da xoa');
-            res.redirect('view');
-        } else {
-            console.log('Sida');
-        }
-    })
-};
+module.exports = function (app, passport) {
 
-// View Account Settings
-exports.setting = function (req, res) {
-    User.findOne({username: 'trungnmse90088'}, function (err, User) {
-        if (err) console.log('Error findOne');
-
-        res.render('setting', { title: 'Account Settings', user: User});
-
-    })
-}
-
-// POST edit User
-exports.edit = function (req, res) {
-    User.findById(id, function (err, User) {
-        if (err) return handleError(err);
-
-        User.email = req.body.email;
-        User.firstname = req.body.firstname;
-        User.lastname = req.body.lastname;
-
-        User.save(function (err) {
-            if (err) return handleError(err);
-            res.redirect('view');
-        });
-    })
-
-}
-
-// POST add user
-exports.submitUser = function (req, res) {
-    var validateMessage = validate(req.body.fullname, req.body.username, req.body.email, req.body.password, req.body.confirmPassword, req.body.date, req.body.month, req.body.year, req.body.gender);
-    if (validateMessage === '') {
-        var user = new User({
-            username: req.body.username,
-            password: req.body.password,
-            fullname: req.body.fullname,
-            email: req.body.email,
-            birthday: new Date(req.body.year, req.body.month, req.body.date),
-            gender: req.body.gender,
-            provider: "local"
-        });
-
-        user.save(function (err, user) {
-            if (err) {
-                var errorMessage = helper.displayMongooseError(err);
-                return res.send(500, errorMessage);
+    // =================================================================================
+    // GET: /view - View All User
+    app.get('/view', function (req, res) {
+        User.find(function (err, data) {
+            if (!err) {
+                res.render('users/view', { title: 'View All User', data: data});
+            } else {
+                console.log('Error');
             }
+        })
+    });
 
-            req.session.user = {
-                id: user.get('id'),
-                fullName: user.get('fullName'),
-                provider: user.get('provider')
-            };
 
-            return res.send(200, 'OK');
-        });
-    } else {
-        res.send(500, validateMessage);
-    }
+    // =================================================================================
+    // GET: /profile - View User Profile
 
-};
+    app.get('/profile', function (req, res) {
+        var title = "Profile";
+        var provider = "";
 
-// Get : Display Add new user page
-exports.signup = function (req, res) {
-    var models = {
-        title: 'Register',
-        dates: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31],
-        months: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
-        years: []
-    };
-    models.years = getAllYears();
+        // Get userID from Session
+        if (req.session.user) {
+            // is logged in
+            title = "Profile of user " + req.session.user.fullName + " of " + req.session.user.provider;
+            provider = req.session.user.provider;
+            User.findOne({'_id': req.session.user.id}, function (err, user) {
+                if (err) return console.log(err);
 
-    return res.render('signup', models);
-};
+                if (user) {
+                    return res.render('users/profile', {title: title, user: user, provider: provider});
+                }
+            });
+        } else {
+            return res.render('users/profile', {title: title, user: "", provider: provider});
+        }
+    });
 
-// Get changPass
-exports.changePass = function (req, res) {
-    res.render('changepass', {title: 'Change Password'})
-}
+    // =================================================================================
+    // GET: /changeinfo - View change profile page
+    app.get('/changeinfo',function (req, res) {
+        // Get userID from Session
+        console.log('DM gay');
+        // Create birthday selection
+        var models = {
+            title: 'Register',
+            dates: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31],
+            months: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+            years: []
+        };
 
-// Post changPass
+        if (req.session.user) {
+            // is logged in
+            title = "Change Profile of user " + req.session.user.fullName + " of " + req.session.user.provider;
+            provider = req.session.user.provider;
+            User.findOne({'_id': req.session.user.id}, function (err, user) {
+                if (err) return console.log(err);
 
-exports.submitPass = function (req, res) {
-    var validateMessage = validatePass(req.body.current, req.body.password, req.body.confirmPassword, req.session.user._id);
-    if (validateMessage === '') {
-    }
-    console.log(validateMessage);
-    res.redirect('view');
+                if (user) {
+                    return res.render('users/changeinfo', {title: title, user: user, provider: provider, models: models});
+                }
+            });
+        } else {
+            return res.render('users/changeinfo', {title: 'Aloha', user: "", provider: provider, models: models});
+        }
+    });
 
-}
+    // =================================================================================
+    // Post: /profile - change profile action
+    app.post('/submitChange', function (req, res) {
+        // Get userID from Session
+        console.log('Body: 2 ' + req.body);
 
-exports.loginTest = function (req, res) {
-    var title = 'Express';
-    var isLoggedIn = false;
-    if (req.session.user) {
-        title = req.session.user.fullName + " of " + req.session.user.provider;
-        isLoggedIn = true;
-    }
-    return res.render('indexLoginTest', { title: title, isLoggedIn: isLoggedIn});
-};
+//    User.findById(req.session._id, function (err, User) {
+//        if (err) return handleError(err);
+//
+//        User.email = req.body.email;
+//        User.firstname = req.body.firstname;
+//        User.lastname = req.body.lastname;
+//        User.birthday = req.body.birthday;
+//        User.gender = req.body.gender;
+//
+//        User.save(function (err) {
+//            if (err) return handleError(err);
+//            res.redirect('view');
+//        });
+//    })
+    });
+
+    // =================================================================================
+    // GET: /delete/ - Delete User
+    app.get('/delete/:userID', function (req, res) {
+        var id = req.params.userID;
+        console.log(id);
+        User.remove({_id: id}, function (err) {
+            if (!err) {
+                console.log('Da xoa');
+                res.redirect('view');
+            } else {
+                console.log('Sida');
+            }
+        })
+    });
+
+    // =================================================================================
+    // GET: /edit/ - Edit User
+//    exports.edit = function (req, res) {
+//        User.findById(id, function (err, User) {
+//            if (err) return handleError(err);
+//
+//            User.email = req.body.email;
+//            User.firstname = req.body.firstname;
+//            User.lastname = req.body.lastname;
+//
+//            User.save(function (err) {
+//                if (err) return handleError(err);
+//                res.redirect('view');
+//            });
+//        })
+//
+//    }
+
+    // =================================================================================
+    // GET: /changepass - View change password page
+    app.get('/changepass',function (req, res) {
+        console.log('aloha');
+        res.render('users/changepass', {title: 'Change Password'})
+    });
+
+    // =================================================================================
+    // POST: /changepass - Change password action
+    app.post('/changepass',function (req, res) {
+        var validateMessage = validatePass(req.body.current, req.body.password, req.body.confirmPassword, req);
+        if (validateMessage === '') {
+
+        } else {
+            console.log('Valudate Message:  ' + validateMessage);
+            res.send(500, validateMessage);
+        }
+    });
 
 
 // helper ============================================================================
 
-// Chức năng Validate
+//Chức năng Validate
 function validate(fullname, username, email, password, confirmPassword, date, month, year, gender) {
     console.log('Full name: ' + fullname);
     console.log('Username: ' + username);
@@ -160,12 +173,12 @@ function validate(fullname, username, email, password, confirmPassword, date, mo
     return 'Please input all field.';
 }
 
-// Validate Change Password
-function validatePass(current, password, confirmPassword, id) {
+//Validate Change Password
+function validatePass(current, password, confirmPassword, req) {
     console.log('Current:     ' + current);
     console.log('Password:    ' + password);
     console.log('RePassword:  ' + confirmPassword);
-    console.log('ID:  ' + id);
+    console.log('ID:  ' + req.session.user.id);
 
 
     if (current && password && confirmPassword) {
@@ -180,33 +193,36 @@ function validatePass(current, password, confirmPassword, id) {
         }
 
         // Check current password
-        User.findById(id, function (err, User) {
+        User.findById(req.session.user.id, function (err, User) {
             if (err) return 'Error WTF !';
-
-            console.log('database:  ' + User.password);
             if (current === User.password) {
                 User.password = password;
                 User.save(function (err) {
-                    if (err) return handleError(err);
+                    if (err){
+                        var errorMessage = helper.displayMongooseError(err);
+                        console.log('Check Current Password:  ' + errorMessage)
+                        return res.send(500, errorMessage);
+                    }
+                    return res.send(200, 'OK');
                     console.log('Password Changed');
-
-                } )
-
-            } else return 'Wrong Current Password';
+                })
+            } else {
+                console.log('Sieu nhan');
+                return 'Wrong Current Password';
+            }
         })
-
     } else
         return 'Please input all field'
 
 }
 
 // Get Year
-function getAllYears() {
-    var years = [];
+    function getAllYears() {
+        var years = [];
 
-    for (var i = new Date().getFullYear(); i > new Date().getFullYear() - 110; i--) {
-        years.push(i);
+        for (var i = new Date().getFullYear(); i > new Date().getFullYear() - 110; i--) {
+            years.push(i);
+        }
+        return years;
     }
-
-    return years;
 }
