ea9aa0fc07c74c26a668dad905b59761005f9b24
trungnmse90088 <trungnmse90088@fpt.edu.vn>
2014-01-02 06:22:24 +0000

    Cập nhật 1 số thứ linh tinh

--- models/user.js
+++ models/user.js
@@ -88,10 +88,10 @@ var userSchema = new Schema({
         type: Schema.Types.ObjectId,
         ref: 'FriendRequest'
     }],
-    group:{
+    group:[{
         name: String,
         listUser: [String]
-    },
+    }],
     todoList:{
         name: String,
         type: String,
