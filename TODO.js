9d9b3c8c8669bb471e8c8069b5f1535365eb7aef
trungnmse90088 <trungnmse90088@fpt.edu.vn>
2014-01-06 05:38:47 +0000

    Update chức năng tạo eventDetail Với sự giúp đỡ nhiệt tình từ ThuậnNH

--- TODO.js
+++ TODO.js
@@ -1,10 +1,19 @@
-// TrungNM
-// TODO: Xem lại cách hiển thị Avatar mặc định
-// TODO: Coi lại mấy cái post chuyển thành put
-// TODO: Mới vào người dùng chưa có Avatar ... nên tạo thêm 1 trường avatar khi mới tạo người dùng nếu không sẽ bị sidaa
-// TODO: Coi mấy cái Ajax load trang
+// TrungNM --------------------------------------------------------------------------------------------------------
+// Work
 // TODO: Upload Photo lên EventDetail
-// TODO: Chỉnh lại avatar mặc định cho User
-// TODO: Xem xét lại cách lưu trữ các thể loại để tránh bị ko trùng khớp
+// TODO: Coi mấy cái Ajax load trang
+// TODO: Coi lại mấy cái post chuyển thành put
 // TODO: Coi lại cái search chỗ tạo eventDetail
-// TODO: Cách tổ chức thư mục lưu trữ - Lưu theo mỗi người dùng 1 Fodel ???
+
+// Wondering
+// ASK: Xem lại cách hiển thị Avatar mặc định
+// ASK: Mới vào người dùng chưa có Avatar ... nên tạo thêm 1 trường avatar khi mới tạo người dùng nếu không sẽ bị sidaa
+// ASK: Chỉnh lại avatar mặc định cho User
+// ASK: Xem xét lại cách lưu trữ các thể loại để tránh bị ko trùng khớp
+// ASK: Cách tổ chức thư mục lưu trữ - Lưu theo mỗi người dùng 1 Fodel ???
+// ASK: Schema của eventDetail
+// ASK: fullName hay fullname
+
+
+
+
