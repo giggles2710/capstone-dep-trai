c163a29c1fbc0a187e466a4a495bb27dfe30674c
trungnmse90088@fpt.edu.vn <trungnmse90088@fpt.edu.vn@641260e3-7f25-cf5c-649d-fd4aaedfc06c>
2014-01-06 08:55:13 +0000

    Cái này phải cần thận đấy thay userId --> userID fullname --> fullName
    Còn cái userId trong user --> friend là vẫn giữ nguyên, chờ sử lý sau

--- trunk/models/eventDetail.js
+++ trunk/models/eventDetail.js
@@ -1,9 +1,10 @@
 var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
+//CheckMe: sửa fullname --> fullName,   userId --> userID
 
 var EmbeddedUser = new Schema ({
 	username: String,
-	fullname: String,
+	fullName: String,
 	avatar: String
 });
 // Nghĩa đã sửa lại like và share thành từ EmbeddedUser thành array bình thường
@@ -13,18 +14,18 @@ var EventDetail = new Schema ({
     endTime: Date,
 	description: String,
 	like: [{
-        userId :{
+        userID :{
             type:Schema.Types.ObjectId,
             ref:'User'
         }
     }],
-	user: [{avatar: String, fullname:String, username: String, userId :{type:Schema.Types.ObjectId,ref:'User'}, status: String, inviteRight: Boolean, note: {title: String, content: String, lastUpdate: Date}}],
-	comment: [{username: String, fullname: String, avatar: String, content: String, datetime: Date}],
+	user: [{avatar: String, fullName:String, username: String, userID :{type:Schema.Types.ObjectId,ref:'User'}, status: String, inviteRight: Boolean, note: {title: String, content: String, lastUpdate: Date}}],
+	comment: [{username: String, fullName: String, avatar: String, content: String, datetime: Date}],
 	photo: [String],
 	announcement: String,
 	tag: [String],
 	share: [EmbeddedUser],
-	creator: {avatar: String, fullname:String, username: String,userId :{type:Schema.Types.ObjectId,ref:'User'}, note: {title: String, content: String, lastUpdate: Date}},
+	creator: {avatar: String, fullName:String, username: String,userID :{type:Schema.Types.ObjectId,ref:'User'}, note: {title: String, content: String, lastUpdate: Date}},
 	lastUpdated: {
         type: Date,
         default: Date.now
