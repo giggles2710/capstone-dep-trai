be7ae64a7791ccd9b1c4daf45b54c7c714451eff
nghianvse90078 <nghianvse90078@fpt.edu.vn>
2014-01-07 08:56:36 +0000

    Model - eventDetail.js : Schema Like.ejs

--- routes/events/homePage.js
+++ routes/events/homePage.js
@@ -14,6 +14,7 @@ module.exports = function (app) {
 // get all event relate to current User
     app.get('/myniti', function (req, res) {
         var currentUser = req.session.user;
+        var userID = currentUser.id;
         var friend = [];
         if (currentUser) {
             User.findOne({'_id': req.session.user.id}, function (err, user) {
@@ -25,20 +26,31 @@ module.exports = function (app) {
                     // Tìm User và USer Friends --> array các ID
 
                     var findFriend = {$or: [
-                        {$and: [
-                            {'creator.userID': req.session.user.id},
-                            {'privacy': {$in: ['c', 'o', 'g']}}
-                        ]
+                        //lấy event của mình
+                        {
+                            $and: [
+                                {'privacy': {$in: ['c', 'o' , 'g']}},
+                                {$or: [
+                                    {$and: [
+                                        {'user.userID': userID},
+                                        {'user.status': {$in: ['m', 'a']}}
+                                    ]},
+                                    {'creator.userID': userID}
+                                ]}
+                            ]
                         },
-                        {$and: [
-                            {'creator.userID': {$in: friend}},
-                            {'privacy': {$in: ['c', 'o']}
-                            },
-                            {$and: [
-                                {'user.userID': req.session.user.id},
-                                {'user.status': {$in: ['m', 'a']}}
-                            ]}
-                        ]
+                        // lấy event của bạn
+                        {
+                            $and: [
+                                {'privacy': {$in: ['c', 'o']}},
+                                {$or: [
+                                    {$and: [
+                                        {'user.userID': {$in: friend}},
+                                        {'user.status': {$in: ['m', 'a']}}
+                                    ]},
+                                    {'creator.userID': {$in: friend}}
+                                ]}
+                            ]
                         }
                     ]
                     }
@@ -120,7 +132,7 @@ module.exports = function (app) {
         var eventId = req.params.id;
         var userId = req.session.user.id;
         console.log("UserID: " + userId);
-        console.log("eventID: "+ eventId);
+        console.log("eventID: " + eventId);
         var userName = req.session.user.fullName;
         // find event
         eventDetail.findOne({'_id': eventId}, function (err, event) {
@@ -131,10 +143,10 @@ module.exports = function (app) {
 
             if (event) {
                 // Check User has already liked or not
-                console.log('event:'+event);
+                console.log('event:' + event);
                 var flash = 1;
                 for (var i = 0; i < event.like.length; i++) {
-                    console.log('length:' +event.like.length);
+                    console.log('length:' + event.like.length);
                     if (event.like[i].userID == userId) {
                         // user has already liked this event => unlike it
                         eventDetail.update({'_id': eventId}, {$pull: {like: {'userID': userId}}}, function (err) {
@@ -149,7 +161,7 @@ module.exports = function (app) {
                     else flash = 0;
                 }
                 // user has not liked this => like it
-                if(flash == 0){
+                if (flash == 0) {
                     eventDetail.update({'_id': eventId}, {$push: {like: {'userID': userId, 'name': userName}}}, function (err) {
                         if (err) {
                             console.log(err);
