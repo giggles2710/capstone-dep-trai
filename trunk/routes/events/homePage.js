835229dba3c06b4f42465443f6a5a598bc11bc32
nghianvse90078@fpt.edu.vn <nghianvse90078@fpt.edu.vn@641260e3-7f25-cf5c-649d-fd4aaedfc06c>
2014-01-07 15:50:07 +0000

    Model - eventDetail.js : Schema Like.ejs

--- trunk/routes/events/homePage.js
+++ trunk/routes/events/homePage.js
@@ -54,82 +54,95 @@ module.exports = function (app) {
                         }
                     ]
                     }
+//                eventDetail.find( { $query: findFriend , $orderby: { 'lastUpdated': -1 }} ,function (err, events) {
+//                    console.log(err);
+//                        res.render('event/home', {title: user.fullName, events: events, user: user});
+//                        console.log("event: " + events);
+//                    });
 
-                    eventDetail.find(findFriend, function (err, events) {
+                    eventDetail.find(findFriend).sort('-lastUpdated').limit(2).exec(function (err, events) {
+                        console.log(err);
                         res.render('event/home', {title: user.fullName, events: events, user: user});
-                    })
-
+                        console.log("event: " + events);
+                    });
 
                 }
             )
-            ;
         }
     })
+
+
 
 //==========================================================================================
 // For Post AJAX
     app.post('/myniti', function (req, res) {
         var count = req.body.count;
         var currentUser = req.session.user;
+        var userID = currentUser.id;
+        var friend = [];
         if (currentUser) {
-            User.findOne({'_id': currentUser.id}, function (err, user) {
-                // Get User's friend list from Database
-                if (err) return console.log(err);
-                else {
-                    // get all friends
-                    var friendList = new Array();
+            User.findOne({'_id': req.session.user.id}, function (err, user) {
                     for (var i = 0; i < user.friend.length; i++) {
-                        var tmp = user.friend[i];
-                        if (tmp.isConfirmed) {
-                            friendList[i] = tmp.user;
-                        }
+                        friend.push(user.friend[i].userId)
+                        //console.log(user.friend[i].userId);
                     }
-                    friendList.push(currentUser.id);
+                    console.log('friend:' + friend);
+                    // Tìm User và USer Friends --> array các ID
 
-                    //get events of all friends
-                    for (var i = 0; i < friendList.length; i++) {
-                        //  lấy ra cái event mà : event đó ở chế độ close community hoặc open.Và bạn mình là người tham gia ở dạng member hoặc ask to join,
-                        //  hoặc bạn mình là người tạo.
-                        eventDetail.find({ $and: [
+                    var findFriend = {$or: [
+                        //lấy event của mình
+                        {
+                            $and: [
+                                {'privacy': {$in: ['c', 'o' , 'g']}},
+                                {$or: [
+                                    {$and: [
+                                        {'user.userID': userID},
+                                        {'user.status': {$in: ['m', 'a']}}
+                                    ]},
+                                    {'creator.userID': userID}
+                                ]}
+                            ]
+                        },
+                        // lấy event của bạn
+                        {
+                            $and: [
                                 {'privacy': {$in: ['c', 'o']}},
                                 {$or: [
                                     {$and: [
-                                        {'user.userID': friendList[i]},
+                                        {'user.userID': {$in: friend}},
                                         {'user.status': {$in: ['m', 'a']}}
                                     ]},
-                                    {'creator.userID': friendList[i]}
+                                    {'creator.userID': {$in: friend}}
                                 ]}
-                            ]},//{sort:[['lastUpdated']]},//.sort({'lastUpdated': -1}).limit(2*count),//TODO : đang test nữa nè
-
-                            function (err, event) {
-                                if (err) return console.log("Không tìm được");
-                                else {
-                                    console.log(event);
-                                    res.render('event/home', {events: event});
-
-                                }
-
-                            })
+                            ]
+                        }
+                    ]
                     }
+
+                    eventDetail.find(findFriend).sort('-lastUpdated').limit(2).skip(2*count).exec(function (err, events) {
+                        console.log(err);
+                        res.render('event/home', {title: user.fullName, events: events, user: user});
+                        console.log("event: " + events);
+                    });
 
 
                 }
-            });
-        } else {
-            return res.redirect('/login');
+            )
         }
 
-
     });
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+//=================================================================================================================
     //Test AJAX like
     app.get('/like', function (req, res) {
         res.render('event/like');
     })
 
-    //AJAX like
+    // POST AJAX like
     app.post('/like', function (req, res) {
-        var eventId = req.params.id;
+        var eventId = req.body.id;
         var userId = req.session.user.id;
         console.log("UserID: " + userId);
         console.log("eventID: " + eventId);
@@ -143,25 +156,11 @@ module.exports = function (app) {
 
             if (event) {
                 // Check User has already liked or not
-                console.log('event:' + event);
-                var flash = 1;
-                for (var i = 0; i < event.like.length; i++) {
-                    console.log('length:' + event.like.length);
-                    if (event.like[i].userID == userId) {
-                        // user has already liked this event => unlike it
-                        eventDetail.update({'_id': eventId}, {$pull: {like: {'userID': userId}}}, function (err) {
-                            if (err) {
-                                console.log(err);
-                                return res.send(500, 'Sorry. You are not handsome enough to do this!');
-                            }
-                            return res.send(200, 'Unlike.');
-                        });
-                        break;
-                    }
-                    else flash = 0;
-                }
-                // user has not liked this => like it
-                if (flash == 0) {
+                console.log('like:' + event.like);
+                console.log('length:' + event.like.length);
+
+                // If chưa có ai like hết
+                if(event.like.length == 0){
                     eventDetail.update({'_id': eventId}, {$push: {like: {'userID': userId, 'name': userName}}}, function (err) {
                         if (err) {
                             console.log(err);
@@ -171,10 +170,111 @@ module.exports = function (app) {
                     });
                 }
 
+                else{
+                    var flash = 1;
+                    for (var i = 0; i < event.like.length ; i++) {
+                        if (event.like[i].userID == userId) {
+                            // user has already liked this event => unlike it
+                            eventDetail.update({'_id': eventId}, {$pull: {like: {'userID': userId}}}, function (err) {
+                                if (err) {
+                                    console.log(err);
+                                    return res.send(500, 'Sorry. You are not handsome enough to do this!');
+                                }
+                                return res.send(200, 'Unlike.');
+                            });
+                            break;
+                        }
+                        else flash = 0;
+                    }
+
+                // user has not liked this => like it
+                    console.log('flash: ' + flash);
+                    if (flash == 0) {
+                        eventDetail.update({'_id': eventId}, {$push: {like: {'userID': userId, 'name': userName}}}, function (err) {
+                            if (err) {
+                                console.log(err);
+                                return res.send(500, 'Sorry. You are not handsome enough to do this!');
+                            }
+                            return res.send(200, 'Like.');
+                        });
+                    }
+                }
+
 
             }
         });
     });
 
+
+
+//====================================================================================================
+    // AJAX share
+    app.get('/share', function(req,res){
+        res.render("/event/share");
+    })
+
+   // AJAX post share
+    app.post('/share',function(req,res){
+        var eventId = req.body.id;
+        var userId = req.session.user.id;
+        console.log("UserID: " + userId);
+        console.log("eventID: " + eventId);
+        // find event
+        eventDetail.findOne({'_id': eventId}, function (err, event) {
+            if (err) {
+                console.log(err);
+                return res.send(500, 'Something wrong just happened. Please try again.');
+            }
+
+            if (event) {
+                // Check User has already liked or not
+                console.log('share:' + event.share);
+                console.log('length:' + event.share.length);
+
+                // If chưa có ai share hết
+                if(event.like.length == 0){
+                    eventDetail.update({'_id': eventId}, {$push: {share: {'userID': userId, 'name': userName}}}, function (err) {
+                        if (err) {
+                            console.log(err);
+                            return res.send(500, 'Sorry. You are not handsome enough to do this!');
+                        }
+                        return res.send(200, 'Like.');
+                    });
+                }
+
+                else{
+                    var flash = 1;
+                    for (var i = 0; i < event.like.length ; i++) {
+                        if (event.like[i].userID == userId) {
+                            // user has already liked this event => unlike it
+                            eventDetail.update({'_id': eventId}, {$pull: {like: {'userID': userId}}}, function (err) {
+                                if (err) {
+                                    console.log(err);
+                                    return res.send(500, 'Sorry. You are not handsome enough to do this!');
+                                }
+                                return res.send(200, 'Unlike.');
+                            });
+                            break;
+                        }
+                        else flash = 0;
+                    }
+
+                    // user has not liked this => like it
+                    console.log('flash: ' + flash);
+                    if (flash == 0) {
+                        eventDetail.update({'_id': eventId}, {$push: {like: {'userID': userId, 'name': userName}}}, function (err) {
+                            if (err) {
+                                console.log(err);
+                                return res.send(500, 'Sorry. You are not handsome enough to do this!');
+                            }
+                            return res.send(200, 'Like.');
+                        });
+                    }
+                }
+
+
+            }
+        });
+    })
 
 }
