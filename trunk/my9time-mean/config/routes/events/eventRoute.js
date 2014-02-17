72b4fa0e3be70bd9ae8e2cb0f48be032350ab607
trungnmse90088@fpt.edu.vn <trungnmse90088@fpt.edu.vn@641260e3-7f25-cf5c-649d-fd4aaedfc06c>
2014-02-17 07:04:29 +0000

    Đang code Upload Avatar

--- trunk/my9time-mean/config/routes/events/eventRoute.js
+++ trunk/my9time-mean/config/routes/events/eventRoute.js
@@ -5,7 +5,8 @@ var eventController = require('../../../app/controllers/events/eventControllerAP
 module.exports = function(app){
     // show events
     app.get('/api/event/:id',eventController.showEvent);
-    app.param('id',eventController.getEvent)
+    //TODO: Check lại thử k liên quan nhưng lúc nào cũng có cái "Get event" ở console
+    //app.param('id',eventController.getEvent)
     // create events
     app.post('/api/event', eventController.createEvent);
     // edit events
