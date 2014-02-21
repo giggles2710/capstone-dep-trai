fe1eecace46b7518cc4f173d1140a9fdd58e7976
trungnmse90088 <trungnmse90088@fpt.edu.vn>
2014-02-21 13:46:49 +0000

    Ghép giao diện sơ sơ cái profile

---
+++ my9time-mean/TODO.js
@@ -0,0 +1,13 @@
+// TrungNM --------------------------------------------------------------------------------------------------------
+// Work
+// TODO: Code Upload avatar
+// TODO: Check lại hiển thị avatar ở timeshelf
+// TODO: Thống nhất lại Avatar trong Database
+
+// Wondering
+// ASK: Xem lại cách hiển thị Avatar mặc định
+// ASK: Mới vào người dùng chưa có Avatar ... nên tạo thêm 1 trường avatar khi mới tạo người dùng nếu không sẽ bị sidaa
+// ASK: Chỉnh lại avatar mặc định cho User
+
+
+
