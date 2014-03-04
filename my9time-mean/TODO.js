5ca9a8be48f0ab23e8074ab4895ddfb4d9989b70
trungnmse90088 <trungnmse90088@fpt.edu.vn>
2014-03-04 06:24:06 +0000

    Sửa lỗi ở chức năng Comment Update từ từ sửa lại cho hoàn chỉnh luôn

--- my9time-mean/TODO.js
+++ my9time-mean/TODO.js
@@ -1,16 +1,9 @@
 // TrungNM --------------------------------------------------------------------------------------------------------
 // Work
 // TODO: Code Upload avatar
-// TODO: Check lại hiển thị avatar ở timeshelf
-// TODO: Thống nhất lại Avatar trong Database
-// TODO: Chỉnh sửa lại chức năng Comment
+// TODO: Chỉnh sửa lại chức năng Comment - cái delete Comment sau khi add ak
 // TODO: Check lại validate email "1234@yahoo...com"
 // TODO: Check lại lúc thay đổi Profile avatar bị thay theo ĐCMvc
-
-// Wondering
-// ASK: Xem lại cách hiển thị Avatar mặc định
-// ASK: Mới vào người dùng chưa có Avatar ... nên tạo thêm 1 trường avatar khi mới tạo người dùng nếu không sẽ bị sidaa
-// ASK: Chỉnh lại avatar mặc định cho User
-
-
+// TODO: Check lại mấy cái Embedded User
+// TODO: Lúc tạo mới chỉ ở View chưa có Bind với database, nên khi delete ngay chưa có comment._id
 
