46d3eeb6a641b5408eaec00ebbc894cb9a1d8406
trungnmse90088@fpt.edu.vn <trungnmse90088@fpt.edu.vn@641260e3-7f25-cf5c-649d-fd4aaedfc06c>
2014-03-04 04:25:19 +0000

    Chỉnh sửa lại chức năng Comment //TODO: Làm giao diện cho comment

--- trunk/my9time-mean/public/js/controllers/user/userController.js
+++ trunk/my9time-mean/public/js/controllers/user/userController.js
@@ -6,6 +6,20 @@ var app = angular.module('my9time.user')
     .controller('userController', ['$rootScope', '$location', '$scope', '$http', 'UserSession', 'Users', '$fileUploader', function ($rootScope, $location, $scope, $http, Session, Users, $fileUploader) {
         $scope.global = Session;
         $scope.myDate = new Date();
+        $scope.default = {
+            dates: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31],
+            months: [1,2,3,4,5,6,7,8,9,10,11,12],
+            years: getAllYears()
+        }
+        function getAllYears(){
+            var years = [];
+
+            for(var i=new Date().getFullYear();i > new Date().getFullYear() - 110;i--){
+                years.push(i);
+            }
+
+            return years;
+        }
 
 
         $scope.test = function(){
@@ -33,7 +47,7 @@ var app = angular.module('my9time.user')
         $scope.updateProfile = function () {
             var user = $scope.user;
             user.$update({id: $scope.global.userId}, function (user) {
-                $location.path('profile');
+                $scope.user = user;
             })
         }
 
