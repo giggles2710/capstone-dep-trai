ab0985b2ac01fb980ca347c058ca3a7269dd5d24
trungnmse90088@fpt.edu.vn <trungnmse90088@fpt.edu.vn@641260e3-7f25-cf5c-649d-fd4aaedfc06c>
2014-03-06 02:20:22 +0000

    Cập nhật tính năng Upload nhiều file

--- trunk/my9time-mean/public/js/directives/cropImage.js
+++ trunk/my9time-mean/public/js/directives/cropImage.js
@@ -2,46 +2,53 @@
  * Created by ConMeoMauDen on 02/03/2014.
  */
 
-var app = angular.module('my9time.event', []);
+angular.module('my9time.user', [])
+    .directive('imgCropped', function() {
+        return {
+            restrict: 'E',
+            replace: true,
+            scope: { src:'@', selected:'&' },
+            link: function(scope,element, attr) {
+                var myImg;
+                var clear = function() {
+                    if (myImg) {
+                        myImg.next().remove();
+                        myImg.remove();
+                        myImg = undefined;
+                    }
+                };
+                scope.$watch('src', function(nv) {
+                    clear();
+                    if (nv) {
+                        element.after('<img />');
+                        myImg = element.next();
+                        myImg.attr('src',nv);
 
-app.directive('imgCropped', function() {
-    return {
-        restrict: 'E',
-        replace: true,
-        scope: { src:'@', selected:'&' },
-        link: function(scope,element, attr) {
-            var myImg;
-            var clear = function() {
-                if (myImg) {
-                    myImg.next().remove();
-                    myImg.remove();
-                    myImg = undefined;
-                }
-            };
-            scope.$watch('src', function(nv) {
-                clear();
-                if (nv) {
-                    element.after('<img />');
-                    myImg = element.next();
-                    myImg.attr('src',nv);
-                    $(myImg).Jcrop({
-                            trackDocument: true,
-                            onSelect: function(x) {
-                                scope.$apply(function() {
+                        var temp = new Image();
+                        temp.src = nv;
+                        temp.onload = function() {
+                            var width = this.width;
+                            var height = this.height;
+
+                            $(myImg).Jcrop({
+                                trackDocument: true,
+                                onSelect: function(x) {
+                                    /*if (!scope.$$phase) {
+                                     scope.$apply(function() {
+                                     scope.selected({cords: x});
+                                     });
+                                     }*/
                                     scope.selected({cords: x});
-                                });
-                            }
-                        },
-                        function () {
-                            // Use the API to get the real image size
-                            var bounds = this.getBounds();
-                            boundx = bounds[0];
-                            boundy = bounds[1];
-                        });
-                }
-            });
+                                },
+                                aspectRatio: 1,
+                                setSelect: [0, 0, 200, 200],
+                                trueSize: [width, height]
+                            });
+                        }
+                    }
+                });
 
-            scope.$on('$destroy', clear);
-        }
-    };
-});
+                scope.$on('$destroy', clear);
+            }
+        };
+    })
