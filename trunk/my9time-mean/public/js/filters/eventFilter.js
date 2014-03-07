6e1847feaf5b872ef6cab1a25297bc4198a1ef0b
nghianvse90078@fpt.edu.vn <nghianvse90078@fpt.edu.vn@641260e3-7f25-cf5c-649d-fd4aaedfc06c>
2014-03-07 15:03:50 +0000

    validate date

--- trunk/my9time-mean/public/js/filters/eventFilter.js
+++ trunk/my9time-mean/public/js/filters/eventFilter.js
@@ -14,15 +14,12 @@ angular.module('my9time.event')
                 return formField.$error.minlength && (formField.$dirty || form.attempted);
             }else if(error == 'email'){
                 return formField.$error.email && (formField.$dirty || form.attempted);
-//            }else if(error == 'daterequired'){
-//                return formField.$error.daterequired && ((form.date1.$dirty && form.month1.$dirty && form.year1.$dirty
-//                    && form.hour1.$dirty && form.minute1.$dirty && form.step1.$dirty)||form.attempted);
-//            }else if(error == 'validStart'){
-//                return formField.$error.validStart && ((form.date1.$dirty && form.month1.$dirty && form.year1.$dirty
-//                    && form.hour1.$dirty && form.minute1.$dirty && form.step1.$dirty)||form.attempted);
-//            }else if(error == 'validEnd'){
-//                return formField.$error.validEnd && ((form.date2.$dirty && form.month2.$dirty && form.year2.$dirty
-//                    && form.hour2.$dirty && form.minute2.$dirty && form.step2.$dirty)||form.attempted);
+            }else if(error == 'validStart'){
+                return formField.$error.validStart && ((form.date1.$dirty && form.month1.$dirty && form.year1.$dirty
+                    && form.hour1.$dirty && form.minute1.$dirty && form.step1.$dirty)||form.attempted);
+            }else if(error == 'validEnd'){
+                return formField.$error.validEnd && ((form.date2.$dirty && form.month2.$dirty && form.year2.$dirty
+                    && form.hour2.$dirty && form.minute2.$dirty && form.step2.$dirty)||form.attempted);
             }else if(error == 'unique'){
                 return formField.$error.unique;
             }else if(error == 'pattern'){
