afcb4a2359f7b2ddc021b3aff44e9689b9a58891
nghianvse90078 <nghianvse90078@fpt.edu.vn>
2014-04-20 19:43:29 +0000

    sửa lại một số thứ

--- my9time-mean/public/lib/skel/event-detail.js
+++ my9time-mean/public/lib/skel/event-detail.js
@@ -1,31 +1,22 @@
 
 $("#changeNotice").click(function(){
-    $("#eventAnnouncement").prop('disabled', false);
+    $("#eventAnnouncement").prop('readonly', false);
     $( "#changeNotice").hide();
     $('#submitAnnouncement').show();
     $('#cancelAnnouncement').show();
 })
 
 $("#submitAnnouncement").click(function(){
-    $("#eventAnnouncement").prop('disabled', true);
+    $("#eventAnnouncement").prop('readonly', true);
     $( "#changeNotice").show();
     $('#submitAnnouncement').hide();
     $('#cancelAnnouncement').hide();
 })
 
 $("#cancelAnnouncement").click(function(){
-    $("#eventAnnouncement").prop('disabled', true);
+    $("#eventAnnouncement").prop('readonly', true);
     $( "#changeNotice").show();
     $('#submitAnnouncement').hide();
     $('#cancelAnnouncement').hide();
 })
 
-function toggleStatus() {
-    if ($('#privacy-privacy').is(':checked')) {
-        $('#friend-token :input').attr('disabled', true);
-        $('#invite-token :input').attr('disabled', true);
-    } else {
-        $('#friend-token :input').removeAttr('disabled');
-        $('#invite-token :input').removeAttr('disabled');
-    }
-}
